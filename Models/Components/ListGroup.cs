55e8dd780f64cb6a1cdf558c08ff2bc4b31b064f
Tan Dinh <tan.dinh@niteco.se>
2023-01-19 17:02:51 +0700

    feat: list group

---
+++ Models/Components/ListGroup.cs
@@ -0,0 +1,24 @@
+﻿namespace Waffle.Models.Components
+{
+    public class ListGroup
+    {
+        public ListGroup()
+        {
+            Items = new List<ListGroupItem>();
+        }
+        public IEnumerable<ListGroupItem> Items { get; set; }
+    }
+
+    public class ListGroupItem
+    {
+        public ListGroupItem()
+        {
+            Name = string.Empty;
+            Url = string.Empty;
+            Icon = "fab fa-staylinked";
+        }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
