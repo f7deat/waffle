8cce1a494c2aa829e86f066ccba8fb30c74c3f5c
f7deat <tan.dct9x@gmail.com>
2024-10-02 16:42:31 +0700

    step

---
+++ core/Models/Components/Step.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Waffle.Core.Foundations;
+
+namespace Waffle.Models.Components;
+
+[Display(Name = nameof(Step), Prompt = nameof(Step))]
+public class Step : AbstractComponent
+{
+    [JsonPropertyName("subTitle")]
+    public string? SubTitle { get; set; }
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+    [JsonPropertyName("items")]
+    public List<StepItem> Items { get; set; } = default!;
+}
+
+public class StepItem
+{
+    [JsonPropertyName("icon")]
+    public string? Icon { get; set; }
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+}
