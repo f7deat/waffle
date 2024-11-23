6f1ac010036dba6b75f68cf4f79b60eb16ee0f4e
f7deat <tan.dct9x@gmail.com>
2024-11-23 19:22:47 +0700

    update layout

--- core/Models/Components/Pages/Home.cs
+++ core/Models/Components/Pages/Home.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Waffle.Core.Foundations;
+using Waffle.Models.ViewModels.Products;
 
 namespace Waffle.Models.Components.Pages;
 
 [Display(Name = "Home", Prompt = nameof(Home))]
 public class Home : AbstractComponent
 {
+    public IEnumerable<ProductListItem> Products { get; set; } = new List<ProductListItem>();
 }
--- core/ViewComponents/HomeViewComponent.cs
+++ core/ViewComponents/HomeViewComponent.cs
@@ -4,6 +4,7 @@ using Waffle.Core.Interfaces.IService;
 using Waffle.Models.Components.Pages;
 using Waffle.Core.Options;
 using Microsoft.Extensions.Options;
+using Waffle.Models;
 
 namespace Waffle.ViewComponents;
 
@@ -12,17 +13,28 @@ public class HomeViewComponent : ViewComponent
     private readonly ApplicationDbContext _context;
     private readonly IWorkService _workService;
     private readonly SettingOptions Options;
-    public HomeViewComponent(ApplicationDbContext context, IWorkService workService, IOptions<SettingOptions> options)
+    private readonly IProductService _productService;
+    public HomeViewComponent(ApplicationDbContext context, IWorkService workService, IOptions<SettingOptions> options, IProductService productService)
     {
         _context = context;
         _workService = workService;
         Options = options.Value;
+        _productService = productService;
     }
 
     public async Task<IViewComponentResult> InvokeAsync(Guid workId)
     {
         var home = await _workService.GetAsync<Home>(workId);
         home ??= new Home();
+        var products = await _productService.ListAsync(new ProductFilterOptions
+        {
+            Current = 1,
+            PageSize = 12,
+            Active = true,
+            Locale = "vi-VN"
+        });
+        home.Products = products.Data;
+        
         return View(Options.Theme, home);
     }
 }
