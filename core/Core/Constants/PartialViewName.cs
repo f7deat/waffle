84c3e2083596e8b577af66ded9b2050487c589c3
f7deat <f7deat@gmail.com>
2024-12-08 11:06:15 +0700

    update no data

--- core/Core/Constants/PartialViewName.cs
+++ core/Core/Constants/PartialViewName.cs
@@ -2,8 +2,9 @@
 
 public class PartialViewName
 {
-    public const string Pagination = "/Pages/Shared/Components/Pagination/Default.cshtml"; 
-    public const string Empty = "/Pages/Components/Empty/Default.cshtml"; 
-    public const string Premium = "/Pages/Components/Empty/Premium.cshtml"; 
-    public const string Link = "~/Pages/Shared/Components/Link/Default.cshtml"; 
+    public const string Pagination = "/Pages/Shared/Components/Pagination/Default.cshtml";
+    public const string Empty = "/Pages/Components/Empty/Default.cshtml";
+    public const string NoData = "/Pages/Components/Empty/NoData.cshtml";
+    public const string Premium = "/Pages/Components/Empty/Premium.cshtml";
+    public const string Link = "~/Pages/Shared/Components/Link/Default.cshtml";
 }
