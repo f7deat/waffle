f4803a70761121fb5642ec710c753e01750c5977
f7deat <tan.dct9x@gmail.com>
2025-02-04 08:53:58 +0700

    Add logging to CareerController and redesign job application form
    - Updated CareerController to include ILogService for logging exceptions.
    - Modified ApplyAsync method to log exceptions and redirect users on error.
    - Redesigned job application form in Detail.cshtml to be more user-friendly with a visible file input for resumes.
    - Removed margin-bottom from label elements in shinec.css.

--- core/Controllers/CareerController.cs
+++ core/Controllers/CareerController.cs
@@ -10,7 +10,7 @@ using Waffle.Models;
 
 namespace Waffle.Controllers;
 
-public class CareerController(IJobOpportunityService _jobOpportunityService, IWebHostEnvironment _webHostEnvironment, UserManager<ApplicationUser> _userManager) : BaseController
+public class CareerController(IJobOpportunityService _jobOpportunityService, IWebHostEnvironment _webHostEnvironment, UserManager<ApplicationUser> _userManager, ILogService _logService) : BaseController
 {
     [HttpPost("save")]
     public async Task<IActionResult> SaveAsync([FromBody] JobOpportunity args)
@@ -23,18 +23,26 @@ public class CareerController(IJobOpportunityService _jobOpportunityService, IWe
     [HttpPost("apply"), AllowAnonymous]
     public async Task<IActionResult> ApplyAsync(JobApplication args, IFormFile? resumeFile)
     {
-        if (User.Identity?.IsAuthenticated == false) return Redirect("/user/login");
-        if (resumeFile is null) return BadRequest("Resume file is required.");
-        var user = await _userManager.FindByIdAsync(User.GetId().ToString());
-        if (user is null || string.IsNullOrEmpty(user.UserName)) return BadRequest("User not found.");
-        var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", "resumes", user.UserName);
-        if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
-        using var fileStream = new FileStream(Path.Combine(folderPath, resumeFile.FileName), FileMode.Create);
-        await resumeFile.CopyToAsync(fileStream);
-        args.ResumeFile = $"https://{Request.Host.Value}/uploads/resumes/{user.UserName}/{resumeFile.FileName}";
-        var result = await _jobOpportunityService.ApplyAsync(args);
-        if (!result.Succeeded) return BadRequest(result.Message);
-        return Redirect("/career/thank");
+        try
+        {
+            if (User.Identity?.IsAuthenticated == false) return Redirect("/user/login");
+            if (resumeFile is null) return BadRequest("Resume file is required.");
+            var user = await _userManager.FindByIdAsync(User.GetId().ToString());
+            if (user is null || string.IsNullOrEmpty(user.UserName)) return BadRequest("User not found.");
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "uploads", "resumes", user.UserName);
+            if (!Directory.Exists(folderPath)) Directory.CreateDirectory(folderPath);
+            using var fileStream = new FileStream(Path.Combine(folderPath, resumeFile.FileName), FileMode.Create);
+            await resumeFile.CopyToAsync(fileStream);
+            args.ResumeFile = $"https://{Request.Host.Value}/uploads/resumes/{user.UserName}/{resumeFile.FileName}";
+            var result = await _jobOpportunityService.ApplyAsync(args);
+            if (!result.Succeeded) return BadRequest(result.Message);
+            return Redirect("/career/thank");
+        }
+        catch (Exception ex)
+        {
+            await _logService.ExceptionAsync(ex);
+            return Redirect("/career/thank");
+        }
     }
 
     [HttpGet("list-opportunity")]
