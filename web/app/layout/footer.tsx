d494b737f0341dfd5f82e0c5b49086d7aeac2f06
f7deat <f7deat@gmail.com>
2025-02-16 22:09:28 +0700

    Add initial project setup with Next.js, Tailwind CSS, and ESLint configuration

---
+++ web/app/layout/footer.tsx
@@ -0,0 +1,21 @@
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div className="flex">
+                <div className="h-12 flex items-center justify-center bg-slate-900 text-white w-12 hover:text-blue-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-5 h-5" fill="currentColor">
+                        <path d="M0 93.7l183.6-25.3v177.4H0V93.7zm0 324.6l183.6 25.3V268.4H0v149.9zm203.8 28L448 480V268.4H203.8v177.9zm0-380.6v180.1H448V32L203.8 65.7z" />
+                    </svg>
+                </div>
+                <div className="w-96">
+                    <input type="search" placeholder="Search" className="w-full h-12 px-4 bg-slate-800 text-white" />
+                </div>
+                <div className="flex-1 bg-slate-900">
+
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
